0118191cd361488364eb40df10747d6cd888697e
jonnyb <jonathan.best@student.kit.edu>
2012-07-18 11:08:16 +0200

    * (temporally) changes of the configuration of the smt-file (the proof obligation) to get unsat core. The unsat core is a usually the "minimal" set of assertion that produces unsat. It is helpful (if minimal/small) for finding wrongly produced inconsistencies. (squashed from Achraf's branch)

--- src/edu/kit/asa/alloy2key/key/KeYFile.java
+++ src/edu/kit/asa/alloy2key/key/KeYFile.java
@@ -19,6 +19,7 @@ import edu.kit.asa.alloy2key.util.Util;
  * 
  * @author Ulrich Geilmann
  * @author Jonny
+ * @author Aboubakr Achraf El Ghazi
  *
  */
 public class KeYFile {
@@ -167,9 +168,13 @@ public class KeYFile {
 //		for (String s : includes)
 //			out.println ("\\include \""+s+"\";");
 //		for (KeYModule m: modules) {
-//			out.println ("\\include \"theory/"+m.filename()+"\";");
+//			out.println ("\\includee \"theory/"+m.filename()+"\";");
 //		}
-		out.println ("(set-logic UFBV)\n(set-option :macro-finder true)");
+		out.println ("(set-logic AUFLIA)\n(set-option :macro-finder true)");
+		
+		// Only for sanity checks
+		out.println ("(set-option :produce-unsat-cores true)");
+		
 		//
 		out.println (";; sorts");
 		out.println (Util.join(sorts, "\n"));
@@ -178,14 +183,19 @@ public class KeYFile {
 		out.println (Util.join(funcs, "\n"));
 		out.println (";; --end functions\n");
 		out.println (";; assertions");
+		
+		int i = 0;
 		for (Term a : asserts) {
-			out.println (String.format("(assert\n  %s\n)", a.toString()));
+			out.println (String.format("(assert \n (! \n  %s \n %s \n ) \n )", a.toString(), ":named a" + i++));
 		}
+		
 		out.println (";; --end assertions\n");
 		out.println (";; lemmas");
+		
 		for (Term a : lemmas) {
-			out.println (String.format("(assert\n  %s\n)", a.toString()));
+			out.println (String.format("(assert\n (! \n  %s \n %s \n ) \n )", a.toString(), ":named a" + i++));
 		}
+		
 		out.println (";; --end lemmas\n");
 		
 		out.println (";; -- key stuff for debugging --");
@@ -204,6 +214,10 @@ public class KeYFile {
 		out.println (";; -- END key stuff --");
 		
 		out.println ("(check-sat)");
+		
+		// Only for sanity checks
+		out.println ("(get-unsat-core)");
+		
 		out.close();
 	}
 
