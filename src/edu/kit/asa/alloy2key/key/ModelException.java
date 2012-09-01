963aeb0d1702e54e4e900074c8ce42863748bac3
Jonathan Best <jonathan.best@student.kit.edu>
2012-09-01 14:00:31 +0200

    included explanation in model exception

--- src/edu/kit/asa/alloy2key/key/ModelException.java
+++ src/edu/kit/asa/alloy2key/key/ModelException.java
@@ -12,7 +12,11 @@ package edu.kit.asa.alloy2key.key;
 @SuppressWarnings("serial")
 public class ModelException extends Exception {
 
-	public ModelException(String string) {
-		super(string);
+	final static String Explanation = " (This exception indicates a problem with the construction of the SMT model." +
+			" Its purpose is to alert the developer of possible bugs and to allow " +
+			"sanity testing inside the model. The end-user should never, ever see this Exception.)";
+	
+	public ModelException(String string) {		
+		super(string + Explanation);
 	}
 }
