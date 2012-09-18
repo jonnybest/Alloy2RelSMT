4da37ab0140186814a4955b21c783c31e5dd9bf8
jonnyb <jonathan.best@student.kit.edu>
2012-09-18 13:20:57 +0200

    added todo

--- src/edu/kit/asa/alloy2key/Translator.java
+++ src/edu/kit/asa/alloy2key/Translator.java
@@ -464,8 +464,8 @@ public class Translator implements Identifiers {
 			if (s.isOrdering()) {
 				Ordering o = (Ordering)s;
 				target.addFunction("Rel1", o.getId());
-				//target.addFunction("Rel2", o.getOrdering());
-				
+				//target.addFunction("Rel2", o.getPath());
+				// TODO: link ordering path with Instance ID
 			} 
 		}
 	}
--- src/edu/kit/asa/alloy2key/modules/Ordering.java
+++ src/edu/kit/asa/alloy2key/modules/Ordering.java
@@ -43,7 +43,7 @@ public class Ordering extends KeYModule {
 	 * Gets the alias expression of the ordering instance
 	 * @return
 	 */
-	public String getOrdering(){
+	public String getPath(){
 		return mod.path();
 	}
 	
