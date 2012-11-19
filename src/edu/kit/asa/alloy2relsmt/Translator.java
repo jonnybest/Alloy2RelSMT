ab2c8d668396e56ac8a30c18bd8503855e135bcd
Jonny Best <jonathan.best@student.kit.edu>
2012-11-19 20:36:40 +0100

    changed translator to return mutual inclusions instead of = (for all non-boolean, non-integer expression)

--- src/edu/kit/asa/alloy2relsmt/Translator.java
+++ src/edu/kit/asa/alloy2relsmt/Translator.java
@@ -944,9 +944,29 @@ public class Translator implements Identifiers {
 				target.getTheory().declareProduct(ar1, ar2);
 				return Term.call("prod_"+ar1+"x"+ar2, e1, e2);
 			case EQUALS:                                             // e1 = e2
-				return e1.equal(e2);
+			{
+				if(be.left.type().is_bool || be.left.type().is_int)
+				{
+					return e1.equal(e2);
+				}
+				else 
+				{
+					target.getTheory().declareSubset(ar1);
+					return Term.call("subset_" + ar1, e1, e2).and(Term.call("subset_" + ar1, e2, e1));
+				}
+			}
 			case NOT_EQUALS:                                         // e1 != e2
-				return e1.equal(e2).not();
+			{
+				if(be.left.type().is_bool || be.left.type().is_int)
+				{
+					return e1.equal(e2).not();
+				}
+				else 
+				{
+					target.getTheory().declareSubset(ar1);
+					return Term.call("subset_" + ar1, e1, e2).and(Term.call("subset_" + ar1, e2, e1)).not();
+				}
+			}
 			case IMPLIES:                                            // c1 => c2
 				return e1.implies(e2);
 			case IN:                                                 // e1 in e2
