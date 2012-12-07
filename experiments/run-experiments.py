a7006aa86b7123c3925ba1d41cacdfd36b05ba17
Jonny Best <jonathan.best@student.kit.edu>
2012-12-07 22:45:24 +0100

    fix for windows

--- experiments/run-experiments.py
+++ experiments/run-experiments.py
@@ -1,31 +1,64 @@
+#!/usr/bin/python
 # 1. traverse folders with python - ok
 # 2. look at each file and print the content -ok
 # 3. write the content to a central log file
 
 import os
 from os.path import join
+import subprocess
 #from z3 import *
 
 thisfile = os.path.realpath(__file__)
 thispath = os.path.dirname(thisfile )
+z3cmd = "~/Programs/z3/build/z3"
+z3args = " -st -v:1 -smt2 -T:550 "
+alloy2relsmtcmd = "java"
+alloy2relsmtparam = " -jar ~/Programs/alloy2RelSMT/alloy2relsmt.jar -f "
+resultfilename = "resultfile.txt"
 
 print "This path is :" + thispath
 print "This directory is known as :" + os.curdir
 print "We run in file: " + __file__
 os.chdir(thispath)
 
-with open("experiment.log", "w") as resultfile:
-    
-    for dirpath, dirnames, filenames in os.walk(thispath, topdown=False):
-        print "new folder found"
-        if '.git' in dirnames:
-            dirnames.remove('.git')
-        print dirpath
-        print dirnames
-        print filenames
-        for file in filenames:
-            file = join(dirpath, file)
-            with open(file, "r") as myfile:
-                resultfile.write ( "I found some text in " + myfile.name + "\n") #str(myfile.read()))
-    resultfile.write("\n")
+os.remove(resultfilename)
+
+for dirpath, dirnames, filenames in os.walk(thispath, topdown=False):
+# mitigate git archives
+    if '.git' in dirpath:
+        continue 
+    if '.git' in dirnames:
+        dirnames.remove('.git')
+
+# debugging messages
+    print "new folder found"
+    print dirpath
+    print dirnames
+    print filenames
+# start doin real stuff
+    for file in filenames:
+# skip bad files
+        if  ".log" in file:
+            continue
+        if  ".txt" in file:
+            continue
+        if ".smt2" in file:
+            continue
+# prepare arguments
+        filepath = join(dirpath, file)
+# open the log file
+        with open(resultfilename, "a") as resultfile:
+# run alloy2relsmt
+            smtfile = file + ".smt2"
+            print [alloy2relsmtcmd, alloy2relsmtparam + filepath + " " +join(dirpath, smtfile)]
+            output = subprocess.check_output([alloy2relsmtcmd + alloy2relsmtparam + filepath + " " + join(dirpath, smtfile)], shell=True)
+            resultfile.write("--- " + file + " ---\n")
+            resultfile.writelines(output)
+            print output
+# run z3 on resulting file
+            output = subprocess.check_output([z3cmd + z3args + join(dirpath, smtfile)], shell=True)
+            resultfile.writelines(output)
+            print output
+# end of doing stuff to file
+            resultfile.write("\n")
 print "done"
